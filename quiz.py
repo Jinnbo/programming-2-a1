f5814569c602bfaa66b495bedc3f9505d398aad0
Tim Ubial <tubial@sd38.bc.ca>
2020-12-08 10:27:51 -0800

    Added one question to Quiz

--- quiz.py
+++ quiz.py
@@ -1,5 +1,15 @@
-# Quiz Game
-# Author:
-# Date:
+# quiz.py
 
-pass
+correct_answer_count = 0
+
+# First Question
+answer = int(input("What's 1 + 1? "))
+
+if answer == 2:
+    print("You're correct!")
+    correct_answer_count += 1
+
+if correct_answer_count > 1:
+    print(f"You got {correct_answer_count} questions right!")
+else:
+    print(f"You got {correct_answer_count} question right!")
