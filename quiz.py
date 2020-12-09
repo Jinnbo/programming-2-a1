92a71fd309a9ef9d82f1e0e99df0a3de44e64800
Tim Ubial <tubial@sd38.bc.ca>
2020-12-09 09:43:23 -0800

    Added question to quiz.py

--- quiz.py
+++ quiz.py
@@ -1,5 +1,21 @@
-# Quiz Game
-# Author:
-# Date:
+# quiz.py
 
-pass
+answers_correct = 0
+
+# Ask a question
+print("Who is the coolest teacher in the world?")
+
+# Get their answer
+answer = input().lower().strip("!,.? ")
+
+# See if they're right
+if answer in ["mr. allan", "bill nye"]:
+    print("You got it right!")
+    answers_correct += 1
+else:
+    print("Sorry, not sorry. It's either Mr. Allan or Bill Nye.")
+
+if answers_correct != 1:
+    print(f"You got {answers_correct} answers correct!")
+else:
+    print(f"You got {answers_correct} answer correct!")
